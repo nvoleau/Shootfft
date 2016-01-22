4ec63579c03904f3403ad8354583503f35f65d91
nvoleau <n.voleau@groupe-pomona.fr>
2016-01-22 15:36:02 +0100

    material design

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,52 +1,58 @@
 angular.module('app.routes', [])
 
- .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('auth', {
-      url: "/auth",
-      abstract: true,
-      templateUrl: "templates/auth.html"
-    })
-    .state('auth.signin', {
-      url: '/signin',
-      views: {
-        'auth-signin': {
-          templateUrl: 'templates/auth-signin.html',
-          controller: 'SignInCtrl'
-        }
-      }
-    })
-    .state('auth.signup', {
-      url: '/signup',
-      views: {
-        'auth-signup': {
-          templateUrl: 'templates/auth-signup.html',
-          controller: 'SignUpCtrl'
-        }
-      }
-    })
-    .state('bucket', {
-      url: "/bucket",
-      abstract: true,
-      templateUrl: "templates/bucket.html"
-    })
-    .state('bucket.list', {
-      url: '/list',
-      views: {
-        'bucket-list': {
-          templateUrl: 'templates/bucket-list.html',
-          controller: 'myListCtrl'
-        }
-      }
-    })
-    .state('bucket.completed', {
-      url: '/completed',
-      views: {
-        'bucket-completed': {
-          templateUrl: 'templates/bucket-completed.html',
-          controller: 'completedCtrl'
-        }
-      }
-    })
+config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('auth', {
+            url: "/auth",
+            abstract: true,
+            templateUrl: "templates/auth.html"
+        })
+        .state('auth.signin', {
+            url: '/signin',
+            views: {
+                'auth-signin': {
+                    templateUrl: 'templates/auth-signin.html',
+                    controller: 'SignInCtrl'
+                }
+            }
+        })
+        .state('auth.signup', {
+            url: '/signup',
+            views: {
+                'auth-signup': {
+                    templateUrl: 'templates/auth-signup.html',
+                    controller: 'SignUpCtrl'
+                }
+            }
+        })
+        .state('bucket', {
+            url: "/bucket",
+            abstract: true,
+            templateUrl: "templates/bucket.html"
+        })
+        .state('bucket.list', {
+            url: '/list',
+            views: {
+                'bucket-list': {
+                    templateUrl: 'templates/bucket-list.html',
+                    controller: 'myListCtrl'
+                }
+            }
+        })
+        .state('seances', {
+            url: '/seances/:key',
+                    templateUrl: 'templates/seance-list.html',
+                    controller: 'SeancesCtrl'
+        })
+
+        .state('bucket.completed', {
+            url: '/completed',
+            views: {
+                'bucket-completed': {
+                    templateUrl: 'templates/bucket-completed.html',
+                    controller: 'completedCtrl'
+                }
+            }
+        })
     $urlRouterProvider.otherwise('/auth/signin');
 });
